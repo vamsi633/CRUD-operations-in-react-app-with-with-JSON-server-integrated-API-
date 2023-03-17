4e0f2599c34093e78cc814dc915e6f445a7bde49
Vamsi krishna nouluri <92721694+vamsi633@users.noreply.github.com>
2023-03-17 11:43:37 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import { useState,useEffect } from "react";
+import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
+import axios from 'axios';
+
+function App() {
+  const [books,setBooks]=useState([]);
+
+  const fetchBooks=async()=>{
+    const response=await axios.get('http://localhost:3001/books');
+    setBooks(response.data);
+  };
+
+  useEffect(()=>{
+    fetchBooks();
+  },[]);
+
+  const editBookById= async(id,newTitle)=>{
+    const responce=await axios.put(`http://localhost:3001/books/${id}`,{
+      title:newTitle
+    });
+    console.log(responce);
+    const updatedBooks=books.map((book)=>{
+      if(book.id===id){
+        return {...book, ...responce.data};
+      }
+
+      return book;
+    });
+
+    setBooks(updatedBooks);
+  };
+
+  const deleteBookById=async(id)=>{
+    await axios.delete(`http://localhost:3001/books/${id}`)
+    const updatedBooks=books.filter((book)=>{
+      return book.id!==id;
+    });
+    setBooks(updatedBooks);
+  };
+
+
+  const createBook=async(title)=>{
+    const response=await axios.post('http://localhost:3001/books',{
+      title:title
+    });
+
+   
+    const updatedBooks=[
+      ...books,
+      response.data
+    ];
+  setBooks(updatedBooks)
+  };
+
+  return (
+    <div className="app">
+      <h1>Reading List</h1>
+      <BookList books={books} onDelete={deleteBookById} onEdit={editBookById}/>
+      <BookCreate onCreate={createBook}/>
+    </div>
+  )
+}
+
+export default App;
